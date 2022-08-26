fa1dbe5960c0aec98d909d24360b4ef8be406885
Germain Buchet <germain.buch@gmail.com>
2022-08-26 16:19:40 +0200

    ViewAdapter & ViewHolder don't work !

--- app/src/main/java/com/technipixl/listmaker/ui/main/ListRecyclerViewAdapter.kt
+++ app/src/main/java/com/technipixl/listmaker/ui/main/ListRecyclerViewAdapter.kt
@@ -1,4 +1,28 @@
 package com.technipixl.listmaker.ui.main
 
-class ListRecyclerViewAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListRecyclerViewAdapter:RecyclerView.Adapter<ListSelectionViewHolder>() {
+
+    val listTitles = arrayOf("Shopping List","Chores","Android Tutorials")
+
+    override fun getItemCount(): Int {
+        return listTitles.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
+
+
--- app/src/main/java/com/technipixl/listmaker/ui/main/ListSelectionViewHolder.kt
+++ app/src/main/java/com/technipixl/listmaker/ui/main/ListSelectionViewHolder.kt
@@ -1,7 +1,10 @@
 package com.technipixl.listmaker.ui.main
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class ListSelectionViewHolder(val binding: ListSelectionViewHolder) : RecyclerView(binding.root) {
+
+class ListSelectionViewHolder(val binding: ListSelectionViewHolder) : RecyclerView.ViewHolder(binding.root) {
+
 
 }
